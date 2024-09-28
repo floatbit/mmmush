619ab728ad6849e60700626d26269b053a7b47d5
“Chris <chris@floatbit.com>
2024-09-27 23:08:51 -0400

    added back vector stores

--- includes/assistants.php
+++ includes/assistants.php
@@ -37,12 +37,12 @@ add_action('wp_insert_post', function($post_id, $post, $update) {
                 update_field( 'assistant_id', $response->id, $post->ID );
             }
 
-            // assistant files
+            // vector stores
             $assistant_id = get_field('assistant_id', $post->ID);
-            $files = get_field('files', $post->ID);
-            if ($files) {
+            $vector_stores = get_field('vector_stores', $post->ID);
+            if ($vector_stores) {
                 $vector_store_ids = [];
-                $vector_store_ids[] = get_field('vector_store_id', $files->ID);
+                $vector_store_ids[] = get_field('vector_store_id', $vector_stores->ID);
                 $client = OpenAI::client(CHATGPT_API_KEY);
                 $data  = [
                     'instructions' => $instructions,
@@ -61,15 +61,7 @@ add_action('wp_insert_post', function($post_id, $post, $update) {
                 ];
                 $response = $client->assistants()->modify($assistant_id, $data);
             }
-            // if ($assistant_files) {
-            //     $client = OpenAI::client(CHATGPT_API_KEY);
-            //     foreach($assistant_files as $assistant_file) {
-            //         $file_id = get_field('file_id', $assistant_file->ID);
-            //         $client->assistants()->files()->create($assistant_id, [
-            //             'file_id' => $file_id,
-            //         ]);
-            //     }
-            // }
+
         }
     }
 
@@ -84,50 +76,58 @@ add_action('wp_insert_post', function($post_id, $post, $update) {
             ]);
             // update file id
             update_field('field_66f5c2e748393', $response->id, $post->ID);
-            // create the vector store
-            $vector_store_id = get_field('vector_store_id', $post->ID);
-            $file_ids = [$response->id];
-            if (empty($vector_store_id)) {
-                $client = OpenAI::client(CHATGPT_API_KEY);
-                $response = $client->vectorStores()->create([
-                    'file_ids' => $file_ids,
-                    'name' => $post->post_title,
-                ]);
-                update_field('field_66f5cf94ef09e', $response->id, $post->ID);
-            }
+            // // create the vector store
+            // $vector_store_id = get_field('vector_store_id', $post->ID);
+            // $file_ids = [$response->id];
+            // // create a vector store
+            // if (empty($vector_store_id)) {
+            //     $client = OpenAI::client(CHATGPT_API_KEY);
+            //     $response = $client->vectorStores()->create([
+            //         'file_ids' => $file_ids,
+            //         'name' => $post->post_title,
+            //     ]);
+            //     $vector_store_id = $response->id;
+            //     update_field('field_66f5cf94ef09e', $vector_store_id, $post->ID);
+            // }
+            // // additional files
         }
     }
 
     if ($post->post_type === 'vector-store') {
         if ($post->post_status === 'publish') {
+            $client = OpenAI::client(CHATGPT_API_KEY);
+            $vector_store_id = get_field('vector_store_id', $post->ID);
+            if (empty($vector_store_id)) {
+                $response = $client->vectorStores()->create([
+                    'name' => $post->post_title,
+                ]);
+                update_field('field_66f5cb66169b5', $response->id, $post->ID);
+            }
+            // put files in the vector store
             $files = get_field('files', $post->ID);
-            $file_ids = [];
             if ($files) {
+                // store file ids
+                $file_ids = [];
                 foreach ($files as $file) {
-                    $file_ids[] = get_field('file_id', $file['file']->ID);
+                    $file_ids[] = get_field('file_id', $file->ID);
                 }
-
-                $vector_store_id = get_field('vector_store_id', $post->ID);
-                if (empty($vector_store_id)) {
-                    $client = OpenAI::client(CHATGPT_API_KEY);
-                    $response = $client->vectorStores()->create([
-                        'file_ids' => $file_ids,
-                        'name' => $post->post_title,
-                    ]);
-                    update_field('field_66f5cb66169b5', $response->id, $post->ID);
-                } else {
-                    $response = $client->vectorStores()->files()->create(
-                        vectorStoreId: $vector_store_id,
-                        parameters: [
-                            'file_id' => 'file-fUU0hFRuQ1GzhOweTNeJlCXG',
-                        ]
-                    );
-                    pr($file_ids);
-                    die($vector_store_id);
+                // first delete all files in the vector store
+                $response = $client->vectorStores()->files()->list($vector_store_id);   
+                if (count($response->data) > 0) {
+                    foreach ($response->data as $file) {
+                        if (!in_array($file->id, $file_ids)) {
+                            $client->vectorStores()->files()->delete($vector_store_id, $file->id);
+                        }
+                    }
                 }
+                // add the new files
+                $response = $client->vectorStores()->batches()->create($vector_store_id, [
+                    'file_ids' => $file_ids,
+                ]);
             }
         }
     }
+
 }, 10, 3);
 
 // Delete assistant when post is trashed
@@ -154,6 +154,14 @@ add_action('wp_trash_post', function($post_id) {
         if ($thread_id) {
             $client = OpenAI::client(CHATGPT_API_KEY);
             $response = $client->threads()->delete($thread_id);
+        }
+    }
+
+    if ($post->post_type === 'vector-store') {
+        $vector_store_id = get_field('vector_store_id', $post_id);
+        if ($vector_store_id) {
+            $client = OpenAI::client(CHATGPT_API_KEY);
+            $client->vectorStores()->delete($vector_store_id);
         }
     }
 });
