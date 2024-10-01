3520dea4ea51949a1829de8a8401c44105fd68df
“Chris <chris@floatbit.com>
2024-10-01 19:22:47 -0400

    file create

--- blocks/user-files-create/block.php
+++ blocks/user-files-create/block.php
@@ -25,24 +25,155 @@ if ( ! empty( $block['align'] ) ) {
     $classes .= ' align' . $block['align'];
 }
 
-$assistant_embed_id = get_query_var('AssistantEmbedId');
+$assistant_embed_id = get_query_var('AssistantEmbedId', '');
 
-if ($assistant_embed_id) {
-    $assistant = get_posts([
-        'numberposts' => 1,
-        'post_type'   => 'assistant',
-        'meta_key'    => 'assistant_embed_id',
-        'meta_value'  => $assistant_embed_id
-    ])[0];
-    pr($assistant);
-    echo $assistant_embed_id;
+if (empty($assistant_embed_id)) {
+    print '<script>window.location.href = "/user/assistants";</script>';
+}
+
+$assistant = get_posts([
+    'numberposts' => 1,
+    'post_type'   => 'assistant',
+    'meta_key'    => 'assistant_embed_id',
+    'meta_value'  => $assistant_embed_id
+])[0];
+
+if ($assistant) {
+    $vector_store = get_field('vector_stores', $assistant->ID);
+    $vector_store_id = get_field('vector_store_id', $vector_store->ID);
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        require_once(ABSPATH . 'wp-admin/includes/image.php');
+        require_once(ABSPATH . 'wp-admin/includes/file.php');
+        require_once(ABSPATH . 'wp-admin/includes/media.php');
+
+        $title = sanitize_text_field(strip_tags($_POST['title']));
+        $file = $_FILES['file'];
+        
+        $uploaded_file = wp_handle_upload($file, ['test_form' => false]);
+
+        if (isset($uploaded_file['file'])) {
+            $file_name = basename($uploaded_file['file']);
+            $file_type = wp_check_filetype($uploaded_file['file']);
+
+            // Prepare an array of post data for the attachment.
+            $attachment = [
+                'guid'           => $uploaded_file['url'],
+                'post_mime_type' => $file_type['type'],
+                'post_title'     => $file_name,
+                'post_content'   => '',
+                'post_status'    => 'inherit'
+            ];
+
+            // Insert the attachment.
+            $attach_id = wp_insert_attachment($attachment, $uploaded_file['file']);
+
+            // Include image.php
+            require_once(ABSPATH . 'wp-admin/includes/image.php');
+
+            // Generate the metadata for the attachment, and update the database record.
+            $attach_data = wp_generate_attachment_metadata($attach_id, $uploaded_file['file']);
+            wp_update_attachment_metadata($attach_id, $attach_data);
+
+            // Create a new post of type 'file'
+            $post_data = [
+                'post_title'   => $title,
+                'post_status'  => 'publish',
+                'post_type'    => 'file',
+                'post_author'  => get_current_user_id(),
+            ];
+
+            $new_post_id = wp_insert_post($post_data);
+
+            // Update the ACF field with the attachment ID
+            update_field('field_66f5c2f148394', $attach_id, $new_post_id);
+
+            // get the file
+            $file = get_field('file', $new_post_id);
+
+            // upload file to openai
+            $client = OpenAI::client(CHATGPT_API_KEY);
+            $response = $client->files()->upload([
+                'purpose' => 'assistants',
+                'file' => fopen($file['url'], 'r'),
+            ]);
+            $file_id = $response->id;
+            // update file id
+            update_field('field_66f5c2e748393', $file_id, $new_post_id);
+
+            // add file to vector store post
+            $files = get_field('files', $vector_store->ID);
+            $file_ids = array($new_post_id);
+            if ($files) {
+                foreach ($files as $file) {
+                    $file_ids[] = $file->ID;
+                }
+            }
+            update_field('field_66f76c4f3450d', $file_ids, $vector_store->ID);
+
+            // get files
+            $files = get_field('files', $vector_store->ID);
+            
+            // get file ids
+            $file_ids = [];
+            foreach ($files as $file) {
+                $file_ids[] = get_field('file_id', $file->ID);
+            }
+            // add the new files to the vector store
+            $response = $client->vectorStores()->batches()->create($vector_store_id, [
+                'file_ids' => $file_ids,
+            ]);
+
+            $upload_success = TRUE;
+        }
+    }
 }
 ?>
 
 <div id="<?php echo esc_attr( $id ); ?>" class="<?php echo esc_attr( $classes ); ?>">
     <div class="container">
-        <h2 class="pt-[100px] pb-[100px]">
-            TODO: blocks/user-files-create
-        </h2>
+        <div class="grid gap-x-8 gap-y-12 grid-cols-1">
+
+            <?php if ($upload_success) : ?>
+            <p role="alert" class="alert alert-success">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-6 w-6 shrink-0 stroke-current"
+                    fill="none"
+                    viewBox="0 0 24 24">
+                    <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+                <span><em><?php print $title;?></em> added to your assistant!</span>
+            </p>
+            <?php endif;?>
+
+            <h2>Add a file to <em><?php echo $assistant->post_title; ?></em></h2>
+            <p>Your assistant will use this file to answer queries.</p>
+            <form action="<?php print get_the_permalink(); ?>?AssistantEmbedId=<?php echo $assistant_embed_id; ?>" method="POST" class="max-w-[720px]" enctype="multipart/form-data">
+                <label class="form-control w-full mb-10">
+                    <div class="label">
+                        <span class="label-text"><strong>Title</strong></span>
+                    </div>
+                    <input type="text" name="title" placeholder="e.g. 2024 Q1 Sales Report" class="input input-bordered w-full text-sm" />
+                </label>
+                <label class="form-control w-full mb-10">
+                    <div class="label">
+                        <span class="label-text"><strong>File</strong></span>
+                    </div>
+                    <input type="file" name="file" class="file-input file-input-bordered w-full text-sm" accept=".pdf,.txt,.html" /> 
+                    <div class="label">
+                        <span class="label-text-alt">Accepted file types: PDF TXT HTML</span>
+                        <span class="label-text-alt">Max 15mb</span>
+                    </div>
+                </label>
+                <p>
+                    <button type="submit" class="btn btn-neutral">Save</button>
+                </p>
+            </form>
+        </div>
     </div>
 </div>
--- single-assistant.php
+++ single-assistant.php
@@ -24,7 +24,7 @@
                             $file_name = $the_file['name'];
                             $file_subtype = $the_file['subtype'];
                         ?>  
-                        <li><a href="<?php echo $file_url; ?>"><?php echo $file_name; ?></a> (<?php echo $file_subtype; ?>)</li>
+                        <li><a href="<?php echo $file_url; ?>"><?php echo $file->post_title; ?></a> (<?php echo $file_subtype; ?>)</li>
                     <?php endforeach; ?>
                 </ul>
             </p>
@@ -34,17 +34,42 @@
             </p>
             <?php endif; ?>
             <p>
-                <a href="/user/files/create" class="btn btn-sm">+ Add File</a>
+                <a href="/user/files/create?AssistantEmbedId=<?php echo get_field('assistant_embed_id'); ?>" class="btn btn-sm">+ Add File</a>
             </p>
         </div>
         <div>   
             <h3>Embed Code</h3>
             <?php if ($files) : ?>
-                <p>You can use this assistant in your website by copying and pasting the following code.</p>
+            <p>You can use this assistant in your website by copying and pasting the following code. Look to the right - that's how it will look.</p>
+            <textarea readonly class="textarea textarea-bordered text-sm h-[260px] w-full">
+                <div id="mmmush-embed">
+                    <h3><?php the_title(); ?></h3>
+                    <div id="mmmush-chat-container"></div>
+                </div>
+                <script src="http://mmmush.localhost/embed/thread.js"></script>
+                <script>
+                    document.addEventListener('DOMContentLoaded', function() {
+                        MMMush({
+                            assistantEmbedId: '<?php print get_field('assistant_embed_id'); ?>'
+                        });
+                    });
+                </script>
+            </textarea>
             <?php else: ?>
-                <p>
-                    Upload files to your assistant to enable embedding.
-                </p>
+            <p role="alert" class="alert alert-warning">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-6 w-6 shrink-0 stroke-current"
+                    fill="none"
+                    viewBox="0 0 24 24">
+                    <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+                </svg>
+                <span><a href="/user/files/create?AssistantEmbedId=<?php echo get_field('assistant_embed_id'); ?>">Upload files</a> to your assistant to enable embedding.</span>
+            </p>
             <?php endif; ?>
         </div>
     </div>
