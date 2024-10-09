d1592f13ffbb424a5684e9c0c0d6f7170ecb64b1
“Chris <chris@floatbit.com>
2024-10-09 12:56:17 -0400

    usage placeholder, build

--- parts/section-header.php
+++ parts/section-header.php
@@ -1,6 +1,6 @@
 <header class="navbar bg-base-200 border-b border-gray-200">
   <div class="flex-1">
-    <a href="/" class="btn btn-ghost text-lg">ally<span class="border border-gray-300 inline-block text-sm px-1.5 py-0.5 relative left-[-2px]">BOX</span></a>
+    <a href="/" class="btn btn-ghost text-lg">ally<span class="border border-gray-300 inline-block text-sm px-1.5 py-0.5 relative left-[-2px] top-[1px]">BOX</span></a>
   </div>
   <div class="flex-none">
     <ul class="menu menu-horizontal px-1">
@@ -9,8 +9,9 @@
         <details>
           <summary>Account</summary>
           <ul class="bg-base-100 rounded-t-none p-2 account-menu">
-            <li><a href="/account/profile">Profile</a></li>
+            <li><a href="/account/usage">Usage</a></li>
             <li><a href="/account/plans">Upgrade</a></li>
+            <li><a href="/account/profile">Profile</a></li>
             <li>
               <div class="divider px-2 m-0"></div>
             </li>
